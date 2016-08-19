5e6958d60d5c4440c89dafbdb26f1e08582dda2b
Matteo Scurati <matteo.scurati@gmail.com>
2016-08-19 11:28:00 +0200

    Remove all duplicates

--- parse.rb
+++ parse.rb
@@ -3,25 +3,58 @@ require "nokogiri"
 require "pp"
 require "sanitize"
 
-#output = File.open("output.txt", "w")
+#output = File.open("articles.txt", "w")
 articles = []
+authors = []
 @file = File.open("articoli.xml")
 @doc = Nokogiri::XML(@file)
+
+@doc.xpath("/pma_xml_export/table[@name='wp_users']").each do |auth|
+  author = {}
+
+  wp_id = auth.xpath("column[@name='ID']").text
+  name = auth.xpath("column[@name='user_login']").text
+
+  author[:wp_id] = wp_id
+  author[:name] = name
+
+  authors << author
+end
+
+pp "Totali autori: ", authors.count
+
 @doc.xpath("/pma_xml_export/table[@name='wp_posts']").each do |post|
   article = {}
 
   date_text = post.xpath("column[@name='post_date']").text
-  date_text = date_text[0, 10]
-  date_format = "%Y-%m-%d"
-  date = Date.strptime(date_text, date_format)
+  #date_text = date_text[0, 10]
+  date_format = "%Y-%m-%d %H:%M:%S"
+  date = DateTime.strptime(date_text, date_format)
   title = post.xpath("column[@name='post_title']").text
   content = post.xpath("column[@name='post_content']").text
+  author = post.xpath("column[@name='post_author']").text
 
   article[:title] = title
   article[:date] = date
-  article[:content] = Sanitize.fragment(content, elements: ['a'], attributes: {a: ['href']})
+  article[:content] = content
+  article[:author_id] = author
+  #article[:content] = Sanitize.fragment(content, elements: ['a'], attributes: {a: ['href']})
 
   articles << article
 end
 
-pp articles
+articles.each do |article|
+  authors.each do |author|
+    if article[:author_id] == author[:wp_id]
+      article[:author_name] = author[:name]
+    end
+  end
+end
+
+pp "Totale articoli: ", articles.count
+
+articles.uniq! { |article| article[:content] }
+
+pp "Totale articoli unici: ", articles.count
+
+articles.sort_by { |article| article[:date] }
