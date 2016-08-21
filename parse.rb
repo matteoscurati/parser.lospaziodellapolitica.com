a4c99aa67f2ab7fd880ae98e1768faf5744b2df6
Matteo Scurati <matteo.scurati@gmail.com>
2016-08-21 17:10:34 +0200

    Remove oldest articles version or draft

--- parse.rb
+++ parse.rb
@@ -5,6 +5,7 @@ require "sanitize"
 
 articles = []
 authors = []
+
 @file = File.open("articoli.xml")
 @doc = Nokogiri::XML(@file)
 
@@ -29,10 +30,12 @@ print "Totali autori: ", authors.count, "\n"
     date_text = date_text[0, 10]
     date_format = "%Y-%m-%d"
     date = Date.strptime(date_text, date_format)
+    id = post.xpath("column[@name='ID']").text
     title = post.xpath("column[@name='post_title']").text
     content = post.xpath("column[@name='post_content']").text
     author = post.xpath("column[@name='post_author']").text
 
+    article[:id] = id
     article[:title] = title
     article[:date] = date
     article[:content] = content
@@ -55,12 +58,20 @@ print "Totale articoli: ", articles.count, "\n"
 
 articles.uniq! { |article| article[:content] }
 
-print "Totale articoli unici: ", articles.count, "\n"
-
-articles.sort_by { |article| article[:date] }
-
 articles.delete_if { |article| article[:title].empty? }
 articles.delete_if { |article| article[:content].empty? }
 articles.delete_if { |article| article[:author_id].empty? }
 
+articles = articles.sort_by { |article| article[:id] }.reverse
+
+articles.uniq! { |article| article[:title] }
+
+print "Totale articoli unici: ", articles.count, "\n"
+
 print "Totale articoli validi: ", articles.count, "\n"
+
+articles.each do |art|
+  if art[:title] == "Roberto Baggio, la demografia italiana, il nuovo welfare"
+    pp art[:id]
+  end
+end
